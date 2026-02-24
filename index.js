c1b63d2d03502a10718c4469a71ce40cdc50f9fb
Parikesittt <parikesit.widodo@gmail.com>
2026-02-24 14:21:04 +0700

    wle

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express()
 
 app.use((req, res, next) => {
-    if(true){
+    if(!true){
         next(new Error('Not Authorized'));
         return;
     }
