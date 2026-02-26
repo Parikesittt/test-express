373514e783bd50d4f7cec9a2adb5094fb4cb1135
Parikesittt <parikesit.widodo@gmail.com>
2026-02-26 16:05:14 +0700

    fix pls

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect(uri, {
     .then(() => console.log('✅ Berhasil Konek!'))
     .catch(err => console.error('❌ Masih Error:', err));
 
-app.use(cors());
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], allowedHeaders: ['Content-Type', 'Authorization'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/notes', notesRouter);
