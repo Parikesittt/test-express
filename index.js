aed018a62f09346a9adbf5a8e78ee42aad10a056
Parikesittt <parikesit.widodo@gmail.com>
2026-02-26 16:03:43 +0700

    .

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect(uri, {
     .then(() => console.log('✅ Berhasil Konek!'))
     .catch(err => console.error('❌ Masih Error:', err));
 
-app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], allowedHeaders: ["Content-Type", "Authorization"], credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/notes', notesRouter);
